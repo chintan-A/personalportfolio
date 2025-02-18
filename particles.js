6b19862a705e9463d25210acfa26f9e1630ceddd
CHINTAN_SHAH\chint <chintanshah97120@gmail.com>
2025-02-18 23:29:26 +0530

    no message

--- particles.js
+++ particles.js
@@ -1,17 +1,16 @@
 particlesJS('particles-js', {
   particles: {
     number: {
-<<<<<<< HEAD
+
       value: 50,
       density: {
         enable: true,
-        value_area: 1000
-=======
-      value: 80,
+        value_area: 1000,
+       value: 80,
       density: {
         enable: true,
         value_area: 800
->>>>>>> bdb85b4adc983a01b4b958864f314c336a702201
+
       }
     },
     color: {
@@ -21,39 +20,39 @@ particlesJS('particles-js', {
       type: 'circle'
     },
     opacity: {
-<<<<<<< HEAD
+
       value: 0.3,
       random: false
     },
     size: {
       value: 2,
-=======
+
       value: 0.5,
       random: false
     },
     size: {
       value: 3,
->>>>>>> bdb85b4adc983a01b4b958864f314c336a702201
+
       random: true
     },
     line_linked: {
       enable: true,
       distance: 150,
       color: '#ffffff',
-<<<<<<< HEAD
+
       opacity: 0.2,
-=======
+
       opacity: 0.4,
->>>>>>> bdb85b4adc983a01b4b958864f314c336a702201
+
       width: 1
     },
     move: {
       enable: true,
-<<<<<<< HEAD
+
       speed: 2,
-=======
+
       speed: 6,
->>>>>>> bdb85b4adc983a01b4b958864f314c336a702201
+
       direction: 'none',
       random: false,
       straight: false,
@@ -66,21 +65,20 @@ particlesJS('particles-js', {
     events: {
       onhover: {
         enable: true,
-<<<<<<< HEAD
+
         mode: 'grab'
       },
       onclick: {
-        enable: false
-=======
+        enable: false,
         mode: 'repulse'
       },
       onclick: {
         enable: true,
         mode: 'push'
->>>>>>> bdb85b4adc983a01b4b958864f314c336a702201
+
       },
       resize: true
     }
   },
   retina_detect: true
-}); 
+});
